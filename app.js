d98b479accd377210c51d340a3ba98b22decca7c
Ajat Darojat <ajatdarojat45@gmail.com>
2021-09-16 13:40:51 +0700

    add register feature

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.post("/register", async (req, res) => {
+	try {
+		const { email, password, username } = req.body;
+		const user = await User.create({ email, password, username });
+		ress.status(200).json({ user });
+	} catch (error) {
+		console.log("🚀 ~ file: app.js ~ line 10 ~ app.post ~ error", error);
+	}
+});
+
 app.listen(PORT, () => {
 	console.log("🚀 ~ file: app.js ~ line 6 ~ app.listen ~ PORT", PORT);
 });
